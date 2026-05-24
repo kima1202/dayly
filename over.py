45ec7e3ef267ddd9a8611bb2f73b8cb8a521aa2b
Kima <2406265572@qq.com>
2026-05-24 13:44:35 +0800

    Create over.py

---
+++ over.py
@@ -0,0 +1,38 @@
+fair--fin骨
+fack nutrition
+  head------last
+<--<--<--<
+  B-------gold(mix)B
+
+def wearup();
+  /short/long
+  /tighly/soft
+  color；
+    w(b
+      b't'(b's'
+           p't'(blue's'
+                y't'(b'total'
+#trouser>夹子>shoe  
+def body.ALL();
+  SIZS(cm)
+  clinder:
+    62-70
+    47-54
+    68-72
+def hand.edge();
+  SIZE(cm)
+  first:1
+  other:0.8-0.9
+def hand.T();
+  SIZE(cm)
+  wrist:3-4
+  arm0:4-5
+  arm1:5-6
+
+
+
+monitor error
+renturn false
+
+
+
